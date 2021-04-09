d6f7f75ebf6eff9586f94fa45106a3f55ce7cd5f
Vedran Sisic <vedran.sisic@gmail.com>
2021-04-08 23:35:02 -0400

    Fix Sidebar types

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import { VStack, Box } from '@chakra-ui/react';
 
-const Sidebar: React.FC = ({ children, ...props }) => {
+type SidebarType = {
+  children?: React.ReactNode;
+  px?: (number | string | null)[];
+};
+
+const Sidebar: React.FC<SidebarType> = ({
+  children,
+  px = [0],
+}: SidebarType) => {
   return (
-    <Box width="20%" minWidth="270px" {...props}>
+    <Box width="20%" minWidth="270px" px={px}>
       <VStack borderRadius={5} bg="white" p={6} spacing={4}>
         {children}
       </VStack>
